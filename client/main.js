185bdc6bce30ce4ce10e8c35d1673323a950b0b8
raeeskhan <raees.khan@itu.edu.pk>
2016-10-19 20:46:53 +0500

    Upto details about a site

--- client/main.js
+++ client/main.js
@@ -2,16 +2,39 @@ import { Template } from 'meteor/templating';
 
 import './main.html';
 
+//Router.configure({	
+//	layoutTemplate: 'ApplicationLayout'
+//});
+
+Router.route('/', function() {
+	this.render("website_form");
+	this.render("website_list")
+});
+
+Router.route('/websites/:_id', function () { 
+  this.render("website", {
+    data:function(){
+      return Websites.findOne({_id:this.params._id});
+    }
+  });
+});
 // template helpers 
 	/////
 
 	// helper function that returns all available websites
 	Template.website_list.helpers({
 		websites:function(){
-			return Websites.find({});
-		}
+			return Websites.find({}, {sort: {up:-1}});
+		},
+
 	});
 
+	Template.website_item.helpers({
+		formattedDate:function(){
+			return moment(this.createdOn).format("DD/MM/YY");
+		}
+
+	});
 
 	/////
 	// template events 
@@ -23,8 +46,11 @@ import './main.html';
 			// (this is the data context for the template)
 			var website_id = this._id;
 			console.log("Up voting website with id "+website_id);
-			// put the code in here to add a vote to a website!
-
+			// put the code in here to add a vote to a website!			
+			if(!this.up) {
+				this.up = 0;
+			}		
+			Websites.update({_id:website_id}, {$set: {up:this.up+1}});
 			return false;// prevent the button from reloading the page
 		}, 
 		"click .js-downvote":function(event){
@@ -35,7 +61,10 @@ import './main.html';
 			console.log("Down voting website with id "+website_id);
 
 			// put the code in here to remove a vote from a website!
-
+			if(!this.down) {
+				this.down = 0;
+			}	
+			Websites.update({_id:website_id}, {$set: {down:this.down+1}});
 			return false;// prevent the button from reloading the page
 		}
 	})
@@ -47,10 +76,25 @@ import './main.html';
 		"submit .js-save-website-form":function(event){
 
 			// here is an example of how to get the url out of the form:
+
 			var url = event.target.url.value;
 			console.log("The url they entered is: "+url);
-			
+			var title = event.target.title.value;
+			var description = event.target.description.value;
+
 			//  put your website saving code in here!	
+
+			if(Meteor.user()) {
+				Websites.insert({
+					title:title,
+					url:url,
+					description:description,
+					createdOn:new Date(),
+            		up: 0,
+            		down: 0
+
+				});
+			}
 
 			return false;// stop the form submit from reloading the page
 
--- server/main.js
+++ server/main.js
@@ -12,25 +12,32 @@ Meteor.startup(() => {
     		title:"Goldsmiths Computing Department", 
     		url:"http://www.gold.ac.uk/computing/", 
     		description:"This is where this course was developed.", 
-    		createdOn:new Date()
+    		createdOn:new Date(),            
+
     	});
     	 Websites.insert({
     		title:"University of London", 
     		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
     		description:"University of London International Programme.", 
-    		createdOn:new Date()
+    		createdOn:new Date(),
+            up: 0,
+            down: 0
     	});
     	 Websites.insert({
     		title:"Coursera", 
     		url:"http://www.coursera.org", 
     		description:"Universal access to the world’s best education.", 
-    		createdOn:new Date()
+    		createdOn:new Date(),
+            up: 0,
+            down: 0
     	});
     	Websites.insert({
     		title:"Google", 
     		url:"http://www.google.com", 
     		description:"Popular search engine.", 
-    		createdOn:new Date()
+    		createdOn:new Date(),
+            up: 0,
+            down: 0
     	});
     }
   });
